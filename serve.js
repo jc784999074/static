aaa66b0f5a24826c30468eb9e825765041697be1
jucheng <jucheng@tal.com>
2021-09-01 18:51:15 +0800

    【】s

--- serve.js
+++ serve.js
@@ -1,7 +1,4 @@
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const url = require('url');
 
 http.createServer(function (request, response) {}).listen(8080);
 

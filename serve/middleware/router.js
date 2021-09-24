5ad6c5859e5f9b15211f4894450362e3a57d1deb
jucheng <jucheng@tal.com>
2021-09-24 18:59:09 +0800

    [feature]添加请求

--- serve/middleware/router.js
+++ serve/middleware/router.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
-// 处理每个js文件:
+// add url-route in /controllers:
+
 function addMapping(router, mapping) {
 	for (var url in mapping) {
 		if (url.startsWith('GET ')) {
@@ -11,27 +12,34 @@ function addMapping(router, mapping) {
 			var path = url.substring(5);
 			router.post(path, mapping[url]);
 			console.log(`register URL mapping: POST ${path}`);
+		} else if (url.startsWith('PUT ')) {
+			var path = url.substring(4);
+			router.put(path, mapping[url]);
+			console.log(`register URL mapping: PUT ${path}`);
+		} else if (url.startsWith('DELETE ')) {
+			var path = url.substring(7);
+			router.del(path, mapping[url]);
+			console.log(`register URL mapping: DELETE ${path}`);
 		} else {
 			console.log(`invalid URL: ${url}`);
 		}
 	}
 }
 
-function addControllers(router) {
-	var files = fs.readdirSync(__dirname + '/controllers');
-	var js_files = files.filter(f => {
-		return f.endsWith('.js');
-	});
-
-	for (var f of js_files) {
-		console.log(`process controller: ${f}...`);
-		let mapping = require(__dirname + '/controllers/' + f);
-		addMapping(router, mapping);
-	}
+function addControllers(router, dir) {
+	fs.readdirSync(__dirname + '/' + dir)
+		.filter(f => {
+			return f.endsWith('.js');
+		})
+		.forEach(f => {
+			console.log(`process controller: ${f}...`);
+			let mapping = require(__dirname + '/' + dir + '/' + f);
+			addMapping(router, mapping);
+		});
 }
 
 module.exports = function (dir) {
-	let controllers_dir = dir || 'controllers', // 如果不传参数，扫描目录默认为'controllers'
+	let controllers_dir = dir || '../controllers',
 		router = require('koa-router')();
 	addControllers(router, controllers_dir);
 	return router.routes();
